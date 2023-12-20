b4a2e90694de7db98821967dec77104912292ec4
zGiusee <giusebelforte@gmail.com>
2023-12-20 14:24:05 +0100

    [UPDATE] Bonus exercise in progress

--- js/script.js
+++ js/script.js
@@ -9,14 +9,12 @@ createApp({
         }
     },
     created() {
-        for(let i = 0; i< 10; i++){
+
+        for(let i = 0; i < 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (email) => {
             this.emails.push(email.data.response); 
         });
         }
-        
-    },
-    methods: {
-
+            
     },
 }).mount('#app')
