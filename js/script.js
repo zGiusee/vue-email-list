1dcaddd95edb0f85220fc7f195e19d386e93106d
zGiusee <giusebelforte@gmail.com>
2023-12-20 14:05:53 +0100

    [UPDATE] Base exercise done

--- js/script.js
+++ js/script.js
@@ -4,13 +4,19 @@ const { createApp } = Vue
 createApp({
     data(){
         return {
+            emails: [ ],
 
         }
     },
     created() {
+        for(let i = 0; i< 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (email) => {
+            this.emails.push(email.data.response); 
+        });
+        }
         
     },
     methods: {
-        
+
     },
 }).mount('#app')
